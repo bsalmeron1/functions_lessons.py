25f0b5eb7a82d32e85273b1f7f36355c7cb74a44
bsalmeron1 <bsalmeron1426@gmail.com>
2026-01-27 20:39:31 +0000

    helo bro hard

--- 2_functions.py
+++ 2_functions.py
@@ -3,7 +3,10 @@
 
 # You should only define the function, you should not call it later.
 
-
+# 
+    
+    
+    
 
 
 
@@ -11,13 +14,18 @@
 
 # Create Functions Practice #2
 # Declare a Function: Create a function called welcome that takes a person's name as an argument. This function should print "Welcome {name}!" whenever it is called.
-
+#  name= "fffd"
+#  def welcome(name):
+#     print(f"Welcome {name} !")
 # Create a Variable: Define a variable named name and store any name of your choice in it.
 
 # Note: Only define the function and create the variable. Do not call the function afterwards.
 
+number= 5
+def square(number):
+    print(number **2)
 
-
+    print(square(number))
 
 
 
