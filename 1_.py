9d143cbc447e90e2742bb39240ac30685c20006f
bsalmeron1 <bsalmeron1426@gmail.com>
2026-01-27 20:28:20 +0000

    hardd day

--- 1_.py
+++ 1_.py
@@ -1,14 +1,32 @@
 # Methods, Help & Documentation Practice #1
 # Remove the characters to the left of our main text:
+# function = A block of reusable code 
+#            place () after the function name to invoke it 
 
+#def happy_birthday(name):
+  #  print(f"Happy birthday to {name}!")
+ #   print(f"You are {age} years old!")
+#    print("Happy birthday to you!")
+#    print()
+
+# happy_birthday("Bro") 
+# happy_birthday("Steve") 
+# happy_birthday("Joe ")
 # ,
 
 # :
 
 # %
-
 # _
+#def happy_birthday(name, age):
+ #   print(f"Happy birthday to {name}!")
+  #  print(f"You are {age} years old!")
+   # print("Happy birthday to you!")
+    #print()
 
+#happy_birthday("Bro", 20) 
+#happy_birthday("Steve", 30) 
+#happy_birthday("Joe ", 40)
 # #
 
 # Use the lstrip() method. Print the result to the screen:
@@ -16,11 +34,46 @@
 # ",:_#,,,,,,:::____##Total_ _Pyt%on,,,,,,::#"
 
 # Search the documentation for the requested method to learn how it works. You can use intermediate variables if you need them.
+#def display_invoice(username, amount, due_date):
+   # print(f"Hello{username}")
+  #  print(f"Your bill of ${amount:.2f} is due : {due_date}")
 
-
+#display_invoice("BroCode",42.50, "01/01")
 # Methods, Help & Documentation Practice #2
 # Add the element "orange" as the fourth element of the following list fruits, using the insert() method:
 
+
+#return=statement used to end a function and send a result back to the caller???
+
+# def add (x,y):
+#     z=x + y
+#     return z
+
+# def subtract(x,y):
+#     z=x-y
+#     return z
+
+# def multiply(x,y):
+#     z= x * y
+#     return z
+# def divide(x,y)
+#     z= x/y
+#     return z
+
+# print(add(1,2))
+# print(subtract(1,2))
+# print(multiply(1,2))
+# print(divide(1,2))
+    
+
+
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + "" + last
+
+full_name = create_name("breanna ", "tapia")
+print(full_name)
 # fruits = ["mango", "banana", "cherry", "plum", "grapefruit"]
 
 # Search the documentation for the requested method to know how it works.
